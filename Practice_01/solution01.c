8a6619e3f897aad82b5e66d65f3c504c3608689e
ItsmeRAAAJ <160505876+ItsmeRAAAJ@users.noreply.github.com>
2024-09-01 05:47:11 +0000

    FIrst time pushing in github

---
+++ Practice_01/solution01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    float len,wid;
+
+    printf("Enter the Length of the Rectangle:");
+    scanf("%f",&len);
+
+    printf("Enter the Width of the Rectangle:");
+    scanf("%f",&wid);
+
+    printf("The Area of the Rectangle is : %f\n \n", len*wid);
+
+
+    return 0;
+}
